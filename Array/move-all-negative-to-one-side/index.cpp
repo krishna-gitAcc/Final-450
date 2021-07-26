229f1b8d562b1f275794062daeaf7eec3310b695
Krishna Kumar <krish_n_a@outlook.com>
2021-07-26 11:53:57 +0530

    update the array file

--- Array/move-all-negative-to-one-side/index.cpp
+++ Array/move-all-negative-to-one-side/index.cpp
@@ -1,16 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void 
-
-int main(){
-    int size;
-    cin>>size;
-    int * arr = new int [size];
-    for (int  i = 0; i < size; i++)
-    {
-        cin>>arr[i];
+void movenegativeaside(int *arr, int size) {
+  int lo = 0, hi = size - 1;
+  for (int i = 0; i <= hi; i++) {
+    if (arr[i] < 0) {
+      swap(arr[i], arr[lo]);
+      lo++;
+    } else {
+      swap(arr[i], arr[hi]);
+      hi--;
+      i--;
     }
+  }
+}
 
-    return 0;
+int main() {
+  int size;
+  cin >> size;
+  int *arr = new int[size];
+  for (int i = 0; i < size; i++) {
+    cin >> arr[i];
+  }
+
+  movenegativeaside(arr, size);
+
+  for (int i = 0; i < size; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
 }
